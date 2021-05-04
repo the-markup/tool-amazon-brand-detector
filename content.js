ba43bb8f3e85a24697fb7584bf846679ae8063d1
jeff crouse <jeff@seethroughlab.com>
2021-05-04 09:33:58 -0500

    change stain color

--- content.js
+++ content.js
@@ -170,7 +170,7 @@ function output_products(title, products) {
  */
 function stain(asin) {
     document.querySelectorAll(`div[data-asin='${asin}']`).forEach( p => {
-        p.style.cssText += 'border:1px dashed red;background-color:rgba(255,0,0,0.5)';
+        p.style.cssText += 'border:1px dashed red; background-color:#ff9900;';
     });
 }
 
