b00d61f2801e3cfe9235c17d3ba7a64899a64d23
leon yin <leon.yin@themarkup.org>
2021-11-12 17:27:23 -0500

    adding on off button to chrome

--- popup.js
+++ popup.js
@@ -1,6 +1,5 @@
 console.log("popup.js", window.location.href);
 
-
 // This is needed to open links from popup.html
 window.onclick = function(e) {
     if (e.target.href && e.target.href.startsWith("http"))
@@ -10,17 +9,25 @@ document.getElementById('clear-storage-link').onclick = function() {
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, "clear_storage");
     });
-    //chrome.storage.sync.clear();
 }
-
 document.getElementById('log-storage-link').onclick = function() {
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, "log_storage");
     });
-    //chrome.storage.sync.get(null, (obj) => console.log(obj));
 }
 
-
+// status for ON-OFF toggle.
+var enabled = true;
+// Check Button status (once)
+chrome.storage.sync.get('toggleisExtensionActive', data => {
+    enabled = data.toggleisExtensionActive;
+    if (enabled === undefined) {
+        enabled = true;
+    }
+    console.log("Logged status", enabled)
+    // What is the status?
+    document.getElementById('togBtn').checked = enabled;
+});
 
 /**
  * This kicks things off in popup.js
@@ -80,55 +87,65 @@ function onContent(content) {
         // This is "State 3"
         html += `Found Amazon brands and exclusive products on the page.`;
         document.body.className = 'enabled loaded';
-        // html += `<p>${content.num_on_page} products total. ${content.products.length} are Amazon.</p>`;
-        // html += `<ol>`;
-        // content.products.forEach(p => {
-        //     html += '<li>';
-        //     html += `<a href="${p.link}">${truncate(p.title, 50)}</a> (${p.detection_method})<br>`;
-        //     html +=  `<img src="${p.image_src}" style="width:100px;"/>`
-        //     html += '</li>';
-        // });
-        // html += '</ol>';
-
     } catch (e) {
-        // This is State 4
-        document.body.className = 'enabled notfound';
-        html += `Didn't find Amazon brands or exclusive products on the page.`;
-        // html += `<p>${e.message}</p>`
+        // Not enabled.
+        if (e.message == "Extension Error: problem getting content. Not enabled.") {
+            html += "<p>Extension not enabled.</p>"
+        } else {
+            // This is State 4
+            html += `<h3>error rendering content</h3>`;
+            html += `<p>${e.message}</p>`
+        }
     }
-    // document.classList.add(select);
     document.getElementById('theContent').innerHTML = html;
 };
 
 
-document.addEventListener('DOMContentLoaded', fetchContent);
+document.addEventListener('DOMContentLoaded', function() {
+    // Collect data
+    fetchContent();
 
-// Get all Accordion and Panel
-let accHeading = document.querySelectorAll(".accordion");
-let accPanel = document.querySelectorAll(".accordion-panel");
-
-for (let i = 0; i < accHeading.length; i++) {
-    // Execute whenever an accordion is clicked 
-    accHeading[i].onclick = function() {
-        if (this.nextElementSibling.style.maxHeight) {
-           hidePanels();     // Hide All open Panels 
-        } else {
-           showPanel(this);  // Show the panel
-        } 
+    // Check the extension slider (persistent)
+    document.getElementById('togBtn').onclick = function() {
+        enabled = !enabled;
+        // document.getElementById('togBtn').checked = enabled;
+        console.log("Toggled to", enabled);
+        var isChecked = this.checked;
+        console.log(isChecked);
+        chrome.storage.sync.set({'toggleisExtensionActive': enabled},function() {});
     };
-}
 
-// Function to Show a Panel
+    // check accordions (persistent)
+    document.querySelectorAll(".accordion").forEach((element) =>
+        element.onclick = function() {
+            console.log("click");
+            if (this.nextElementSibling.style.maxHeight) {
+                hidePanels();
+            } else {
+                showPanel(this);
+            } 
+        }  
+    );
+});
+
+
+/**
+ * Function to show a specific panel
+ * @param {*} elem 
+ */
 function showPanel(elem) {
-  hidePanels();
-  elem.classList.add("active");
-  elem.nextElementSibling.style.maxHeight = elem.nextElementSibling.scrollHeight + "px";
+    hidePanels();
+    elem.classList.add("active");
+    elem.nextElementSibling.style.maxHeight = elem.nextElementSibling.scrollHeight + "px";
 }
-
-// Function to Hide all shown Panels
+/**
+ * Function to Hide all shown Panels
+ */
 function hidePanels() {
-  for (let i = 0; i < accPanel.length; i++) {
-      accPanel[i].style.maxHeight = null;
-      accHeading[i].classList.remove("active");
-  }
+    let accPanel = document.querySelectorAll(".accordion-panel");
+    let accHeading = document.querySelectorAll(".accordion");
+    for (let i = 0; i < accPanel.length; i++) {
+        accPanel[i].style.maxHeight = null;
+        accHeading[i].classList.remove("active");
+    }
 }
